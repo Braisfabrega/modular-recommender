9849f467f5f18bb0ea5fc9b03cfbfb775c170ecc
Brais <e.bfabregaf@ies-sabadell.cat>
2026-05-11 12:03:06 +0200

    fix merge

--- marco_version/recommender.py
+++
@@ -1,9 +0,0 @@
-# recommender.py
-
-class Recommender:
-    def __init__(self, loader):
-        # loader conté totes les dades carregades (usuaris, items, ratings)
-        self.loader = loader
-
-    def recommend(self, user_id):
-        raise NotImplementedError("Aquest mètode s'ha d'implementar a les subclasses")
